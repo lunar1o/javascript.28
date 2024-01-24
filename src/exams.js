cfe7be7a7f5d0939059602df09b374ba8ff96426
Isabelle Oliveira <127144808+lunar1o@users.noreply.github.com>
2024-01-23 23:15:18 -0300

    Update exams.js

--- src/exams.js
+++ src/exams.js
@@ -1,6 +1,6 @@
 import months from './months.js'
 
-// Filtra meses
+
 const filteredMonths = months.filter( (months)=> {
     return months.days === 31
 })
@@ -9,23 +9,20 @@ console.log(filteredMonths)
 
 let toPrint = ''
 
-// Lista meses
+
 filteredMonths.forEach(month => {
    toPrint += month.month + ', '
 });
 
-// for( let c = 0; c < filteredMonths.length; c++ ){
+let meses = [30,30,30] 
 
-// }
 
-let meses = [30,30,30] // array comum
+let meses = [30,30,30] 
 
-// Reduce - função com todos os itens do array e devolve um valor único
 let sumMonthDays = filteredMonths.reduce( (prev, next) => {
     return {days: prev.days + next.days}
 })
 
-// Map - funções em arrays de retorno individual (cada valor array)
 let cachorros = [10, 15, 7, 8, 9]
 
 let years = cachorros.map( (cachorro => {
