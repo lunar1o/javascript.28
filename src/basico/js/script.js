c0171869fef67466b8a2735b531e1c46670ea4fe
Isabelle Oliveira <127144808+lunar1o@users.noreply.github.com>
2024-01-23 23:12:04 -0300

    Update script.js

--- src/basico/js/script.js
+++ src/basico/js/script.js
@@ -1,44 +1,47 @@
-function calcularMedia( notas ) {
-
-    let soma = 0;
-    for( c = 0; c < notas.length; c++) {
-        soma += notas[c];
+class calculadora {
+	constructor(notas){
+        this.notas = notas
     }
 
-    media = soma / notas.length;
+  media() {
+  	let valores = this.notas;
+    let media = 0;
+		let c = 0;
+    let soma = 0;
+   
+    for (c = 0; c < valores.length; c++) {
+      soma += valores[c];
+    }
 
-    return media;
 
-}
 
-let media; // escopo global
+    media = soma / valores.length;
 
-function aprovacao( notas ) {
+    return media
+  }
 
-    let media = calcularMedia( notas ); // escopo da função
-
-    let condicao = media >= 8 ? "aprovado" : "reprovado";
-
-    return 'Média: ' + media + ' - Resultado: ' + condicao;
-
+  aprovacao(){
+  	let valor = this.media()
+  	if(valor>=60){
+    	return "Aprovado"
+    }
+    else{
+    	return "Reprovado"
+    }
+  }
 }
 
 
 // Função Recursivas
 
 function contagemRegressiva(numero){
-
     console.log(numero);  
     
     let proximoNumero = numero - 1;
-
     if(proximoNumero > 0)
         contagemRegressiva(proximoNumero);
-
 }
-
 // contagemRegressiva(50);
-
 /* 
  * Formulário envio de dados para cálculo da média 
  */
@@ -46,102 +49,7 @@ const formulario1 = document.getElementById('formulario-01');
 
 if(formulario1)
     formulario1.addEventListener('submit', function( evento ){
-
-        evento.preventDefault();
-        evento.stopPropagation();
-
-        if( this.getAttribute('class').match(/erro/) ) {
-            return false;
-        }
-        
-        let dados = new FormData(this);
-
-        let notas = [];
-
-        for(let key of dados.keys()) {
-
-            let numero = dados.get(key).match(/\d*/) ? Number(dados.get(key)) : 0; // é um número
-
-            if(!isNaN(numero)) {
-                notas.push(numero);
-            }
-
-        }
-
-        console.log(notas);
-
-        texto = aprovacao(notas)
-
-        document.getElementById('resultado').innerHTML = texto;
-
-    });
-
-
-function validaCampo(elemento){
-
-    elemento.addEventListener('focusout', function(event) {
-
-        event.preventDefault();
-
-        if(this.value == ""){
-            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em vermelho";
-            this.classList.add('erro');
-            this.parentNode.classList.add('erro');
-            return false;
-        } else {
-            document.querySelector('.mensagem').innerHTML = "";
-            this.classList.remove('erro');
-            this.parentNode.classList.remove('erro');
-        }
-
-    });
-
-}
-
-function validaCampoNumerico(elemento){
-
-    elemento.addEventListener('focusout', function(event) {
-
-        event.preventDefault();
-
-        let numero = this.value.match(/^[\d]5-[\d]3/) ? this.value.replace(/-/, "") : this.value; 
-
-        if(numero != "" && numero.match(/[0-9]*/) && numero >= 0 && numero <= 10){
-            document.querySelector('.mensagem').innerHTML = "";
-            this.classList.remove('erro');
-            this.parentNode.classList.remove('erro');
-        } else {
-            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
-            this.classList.add('erro');
-            this.parentNode.classList.add('erro');
-            return false;
-        }
-
-    });
-
-}
-
-
-function validaEmail(elemento){
-
-    elemento.addEventListener('focusout', function(event) {
-
-        event.preventDefault();
-
-        const emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
-        if(this.value.match(emailValido)) {
-            document.querySelector('.mensagem').innerHTML = "";
-            this.classList.remove('erro');
-            this.parentNode.classList.remove('erro');
-        } else {
-            document.querySelector('.mensagem').innerHTML = "verifique o preenchimento dos campos em destaque";
-            this.classList.add('erro');
-            this.parentNode.classList.add('erro');
-            return false;
-        }
-
-    });
-
+@@ -145,9 +149,9 @@ function validaEmail(elemento){
 }
 
 
@@ -152,12 +60,9 @@ let camposEmail = document.querySelectorAll('input.email');
 for( let emFoco of camposObrigatorios) {
     validaCampo(emFoco);
 }
-
 for( let emFoco of camposNumericos) {
     validaCampoNumerico(emFoco);
 }
-
 for( let emFoco of camposEmail) {
     validaEmail(emFoco);
 }
-
